6919bd7abcf28fe385e1a5d861834605ccba60be
Phillip Smith <blueshiftps@gmail.com>
2019-01-24 18:46:22 -0500

    Added header using Material UI

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  leftGrow: {
+    flexGrow: 1,
+    textAlign: 'left'
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function Header(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit" className={classes.leftGrow}>
+            Openfin App Manager
+          </Typography>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Discover</Button>
+          <Button color="inherit">Create</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
