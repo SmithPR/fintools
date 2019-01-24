58dd10a4e5c31b192519aff3ff61c8ac51737d3d
Phillip Smith <blueshiftps@gmail.com>
2019-01-24 18:53:54 -0500

    added SVG icon library and baseline CSS

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js';
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <CssBaseline>
+          <Header />
+        </CssBaseline>
       </div>
     );
   }
