e7f13c85fd2fc74aa3a3b71c7159a3cb0e6b0dd6
Phillip Smith <blueshiftps@gmail.com>
2019-01-24 19:08:54 -0500

    Improved header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,26 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 
 const styles = {
   root: {
     flexGrow: 1,
   },
-  leftGrow: {
+  headerBar: {
+      minHeight: '36px'
+  },
+  appTitle: {
     flexGrow: 1,
-    textAlign: 'left'
+    textAlign: 'left',
+    fontSize: '1rem'
   },
   menuButton: {
     marginLeft: -12,
     marginRight: 20,
+    padding: '6px 8px'
   },
 };
 
@@ -25,8 +32,11 @@ function Header(props) {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar variant="dense">
-          <Typography variant="h6" color="inherit" className={classes.leftGrow}>
+        <Toolbar variant="dense" className={classes.headerBar}>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.appTitle}>
             Openfin App Manager
           </Typography>
           <Button color="inherit">Home</Button>
