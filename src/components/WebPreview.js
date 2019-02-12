87fcc92d7a8a0e95812c1f02c470909513695b21
Phillip Smith <blueshiftps@gmail.com>
2019-02-12 17:57:59 -0500

    Formatted page title

--- src/components/WebPreview.js
+++ src/components/WebPreview.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
+import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import DownloadIcon from '@material-ui/icons/GetApp';
+import Divider from '@material-ui/core/Divider';
 
 import green from '@material-ui/core/colors/green';
 
 import MainWindow from './MainWindow.js';
+import LogoIcon from './LogoIconWhite';
 
 const webDirectory = window.location.href.substring(0, window.location.href.lastIndexOf('/'));
 const downloadLocation = `https://install.openfin.co/download/?config=${encodeURI(webDirectory+'/app_'+window.location.hostname+'.json')}&fileName=FinTools%20App%20Manager%20Installer&unzipped=true`
@@ -23,6 +26,7 @@ const styles= theme=>({
         background: 'radial-gradient(#8593e3,#263481)',
         color: '#fff',
         padding: theme.spacing.unit*4,
+        paddingTop: theme.spacing.unit*2,
         overflow: 'hidden'
     },
     previewContainer: {
@@ -52,14 +56,38 @@ const styles= theme=>({
         marginRight: 'auto',
         marginTop: theme.spacing.unit*4,
         marginBottom: theme.spacing.unit*4
+    },
+    appLogo: {
+        fontSize: '3em',
+        marginBottom: '-0.2em',
+        marginRight: theme.spacing.unit
+    },
+    pageTitle: {
+        display: 'inline-block',
+        fontSize: 30,
+        fontWeight: 'normal',
+        margin: 0
+    },
+    divider: {
+        backgroundColor: 'rgba(255,255,255,0.18)',
+        marginTop: theme.spacing.unit*2
     }
 
 });
 
 const WebPreview = ({classes}) => (
     <div className={`WebPreview ${classes.webPreview}`}>
+        <Grid container>
+            <Grid item xs={8}>
+                <LogoIcon className={classes.appLogo}></LogoIcon>
+                <h1 className={classes.pageTitle}>
+                    FinTools App Manager
+                </h1>
+            </Grid>
+            <Grid item xs={4}>Socials</Grid>
+        </Grid>
+        <Divider className={classes.divider} />
         <div>
-            <h1>FinTools App Manager</h1>
         </div>
         <div className={classes.buttonContainer}>
             <Fab variant="extended" color="primary" aria-label="Add" className={classes.downloadButton} href={downloadLocation}>
