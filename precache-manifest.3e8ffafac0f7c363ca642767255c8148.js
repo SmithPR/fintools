69935867e78c5662c9a3c527a6a21524e52e5905
Phillip Smith <blueshiftps@gmail.com>
2019-02-12 19:22:44 -0500

    Updates

---
+++ precache-manifest.3e8ffafac0f7c363ca642767255c8148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/fintools/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ca355a26fa4d9501a5df",
+    "url": "/fintools/static/js/runtime~main.ca355a26.js"
+  },
+  {
+    "revision": "cbafefb0a4dd37a9f35b",
+    "url": "/fintools/static/js/main.cbafefb0.chunk.js"
+  },
+  {
+    "revision": "6b59a8eb8c7bb8f0efdc",
+    "url": "/fintools/static/js/1.6b59a8eb.chunk.js"
+  },
+  {
+    "revision": "cbafefb0a4dd37a9f35b",
+    "url": "/fintools/static/css/main.1ed47aa3.chunk.css"
+  },
+  {
+    "revision": "7310eec95612f5ced3995eb83abc0cd2",
+    "url": "/fintools/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fintools/precache-manifest.99fdf09feaf3852c70919e2f0695469b.js"
+  "/fintools/precache-manifest.3e8ffafac0f7c363ca642767255c8148.js"
 );
 
 workbox.clientsClaim();
